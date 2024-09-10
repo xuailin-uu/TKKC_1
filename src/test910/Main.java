4b236fccd2f3ecdf5052988ad80a277226f0c113
Administrator <3542183560@qq.com>
2024-09-10 22:34:32 +0800

    第一次上传

---
+++ src/test910/Main.java
@@ -0,0 +1,36 @@
+package test910;
+
+import java.util.*;
+
+public class Main {
+    public static void main1(String[] args) {
+        int a,b,c,max;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        max=a;
+
+        if (b > max) {
+            max=b;
+        }
+        if (c>max){
+            max=c;
+        }
+
+
+        System.out.println(max*max);
+
+//        JOptionPane.showMessageDialog(null,"x=3;"+"\ny=5;"+"\nx+y="+sum);
+//        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        int sum=0;
+        while (sum<10){
+            System.out.println(sum);
+            sum++;
+        }
+
+
+}}
